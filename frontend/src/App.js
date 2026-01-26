b432390bdc2ab8d9f2b6f1a4b4bfa7e7733baf71
Haeyoung Lee <hae081128@gmail.com>
2026-01-26 17:55:46 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -61,7 +61,7 @@ function App() {
             <div className="orbit-scene">
               <button className="back-btn" onClick={() => setPage('MAIN')}>← 뒤로가기</button>
               <div className="sun">중심</div>
-              {stars.map((star, index) => (
+              {stars.movies.map((star, index) => (
                 <div 
                   key={star.id} 
                   className="star" 
